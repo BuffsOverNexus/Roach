929be600d75e5a6d54774173d5c4ae8a1587fa47
BuffsOverNexus <buffsovernexus2@gmail.com>
2023-02-01 20:58:39 -0500

    Updated bot page and schema

--- src/index.ts
+++ src/index.ts
@@ -10,53 +10,31 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/todos", async (req, res) => {
-  const todos = await prisma.todo.findMany({
-    orderBy: { createdAt: "desc" },
+app.get("/users", async(req, res) => {
+  const users = await prisma.user.findMany({
+    orderBy: { createdAt: "desc" }
   });
 
-  res.json(todos);
-});
+  res.json(users);
+})
 
-app.post("/todos", async (req, res) => {
-  const todo = await prisma.todo.create({
+app.get("/user/:id", async(req, res) => {
+  const id = req.params.id;
+  const user = await prisma.user.findUnique({
+    where: { id }
+  })
+})
+
+app.post("/user", async (req, res) => {
+  const user = await prisma.user.create({
     data: {
       completed: false,
       createdAt: new Date(),
       text: req.body.text ?? "Empty todo",
     },
   });
-
-  return res.json(todo);
 });
 
-app.get("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.findUnique({
-    where: { id },
-  });
-
-  return res.json(todo);
-});
-
-app.put("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.update({
-    where: { id },
-    data: req.body,
-  });
-
-  return res.json(todo);
-});
-
-app.delete("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  await prisma.todo.delete({
-    where: { id },
-  });
-
-  return res.send({ status: "ok" });
-});
 
 app.get("/", async (req, res) => {
   res.send(
