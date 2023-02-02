b67699f5e27f2d5eb975ab9b025d4fef670dccae
BuffsOverNexus <buffsovernexus2@gmail.com>
2023-02-01 22:29:21 -0500

    Added dotenv and updated bot

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import express from "express";
 const prisma = new PrismaClient();
 const client = new Client({ intents: [] });
 
+require("dotenv").config();
+
 
 const app = express();
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || "localhost";
 
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
@@ -22,12 +23,16 @@ app.get("/users", async(req, res) => {
 });
 
 app.get("/user/:id", async(req, res) => {
-  const id = Number(req.params.id);
-  const user = await prisma.user.findUnique({
-    where: { id }
-  });
-
-  res.json(user);
+  try {
+    const id = Number(req.params.id);
+    const user = await prisma.user.findUnique({
+      where: { id }
+    });
+  
+    res.json(user);
+  } catch {
+    res.status(400).send("Invalid type.");
+  }
 });
 
 app.post("/oauth", async(req, res) => {
@@ -37,15 +42,14 @@ app.post("/oauth", async(req, res) => {
 client.on('ready', () => {
   console.log('Discord bot is ready.');
   app.listen(port, () => {
-    console.log('Server is running on http://${host}:${port}');
+    console.log('Server is running on http://localhost:${port}');
   });
 });
 
-client.on('message', (message: Message) => {
+client.on('message', (message) => {
   if (message.content === 'ping') {
     message.reply('pong');
   }
 });
 
-
-client.login(process.env.BOT_TOKEN);
+client.login();
