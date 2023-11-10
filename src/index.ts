350598247af268245f15b7e58e06b7a5f6e7502e
BuffsOverNexus <buffsovernexus2@gmail.com>
2023-11-10 15:23:49 -0500

    Sends kooper a good morning message at 3:30am daily

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { createRole, getAllChannelsInGuild, getAllEmotesInGuild, getAllGuildsOwn
 import { generateException } from "./util/exception_handling";
 import  cors  from "cors";
 import { regenerateMessage } from "./message/generate_message";
+import cron from "node-cron";
 
 const environment = process.env.RAILWAY_ENVIRONMENT || "local";
 const port = process.env.PORT || 3000;
@@ -215,10 +216,21 @@ client.on('messageReactionRemove', async (reaction, user) => {
     await handleRemoveReaction(prisma, client, reaction, user);
 }); 
 
+
 // When the user deletes a message, ensure it's not a message we care about.
 // Otherwise, delete ALL reaction roles.
 client.on('messageDelete', async (message: Message | PartialMessage) => {
   await handleRemoveMessage(prisma, message);
+});
+
+cron.schedule("30 3 * * *", () => {
+  const user = client.users.cache.get("511334132115308545");
+
+  if (user) {
+    user.send("Good morning!");
+  } else {
+    console.error("User not found!");
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
